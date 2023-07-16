b3668d58ae84a1c6e012f6ca74625bb403a8254f
Denis Mukhin <mukhin88dm@yandex.ru>
2023-07-16 22:20:42 +0300

    4.1.0. Оператор if с блоком else [#227534]

---
+++ src/main/java/ru/job4j/condition/Tour.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Tour {
+    public static void offer(boolean passport) {
+        if (passport) {
+            System.out.println("Tours abroad.");
+        } else {
+            System.out.println("Tours in Russia");
+        }
+    }
+
+    public static void main(String[] args) {
+        Tour.offer(true);
+        Tour.offer(false);
+    }
+}
